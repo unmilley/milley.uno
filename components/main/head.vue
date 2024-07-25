1dda617495dbf974febc1b1e4a5906fedfa03dc5
unmilley <1337@milley.uno>
2024-07-25 18:54:42 +0500

    `feat: add head component to index page`

---
+++ components/main/head.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-end gap-2">
+    <div class="avatar relative">
+      <div class="mask mask-squircle w-24">
+        <NuxtImg
+          src="static/me-gray.png"
+          responsive
+          :placeholder="[50, 50, 75, 1]"
+          preload
+          alt="alexandr"
+          placeholder-class="animate-pulse"
+          @load="isImageLoad = true"
+        />
+      </div>
+      <span class="absolute flex size-3 bottom-1 right-1" :class="{ 'opacity-50': !isImageLoad }">
+        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary/75 z-10"></span>
+        <span class="relative inline-flex rounded-full size-3 bg-primary outline outline-base-100"></span>
+      </span>
+    </div>
+    <p class="whitespace-nowrap font-medium text-sm">Available for part-time</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isImageLoad = ref(false)
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>home</h1>
+  <div ref="main">
+    <ClientOnly>
+      <ui-scroll-bar :target="main" />
+    </ClientOnly>
+    <main-head />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const main = ref<HTMLDivElement | null>(null)
+</script>

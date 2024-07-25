015cf9b9992b1defc1c173ec1799088cf4086a5f
unmilley <1337@milley.uno>
2024-07-25 13:57:21 +0500

    `feat: add nuxt modules: content, image, scripts 📝`

---
+++ content/blog/temp.md
@@ -0,0 +1,7 @@
+---
+title: 'Temp content'
+summary: 'This is temp.md for @nuxt/content'
+publishedAt: '2024-07-25'
+---
+
+# Temp
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ const devServer = {
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false, telemetry: false, timeline: { enabled: false } },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/scripts'],
   tailwindcss: {
     cssPath: ['@/assets/css/tailwind.css', { injectPosition: 'first' }],
     configPath: '@/tailwind.config.ts',

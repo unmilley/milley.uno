5ab61cd2c3e89bd9e438e88da409ae3066bf7af3
unmilley <1337@milley.uno>
2024-07-25 14:02:38 +0500

    `feat: add nuxt module: i18n`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,29 @@ const devServer = {
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false, telemetry: false, timeline: { enabled: false } },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/scripts'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/scripts', '@nuxtjs/i18n'],
   tailwindcss: {
     cssPath: ['@/assets/css/tailwind.css', { injectPosition: 'first' }],
     configPath: '@/tailwind.config.ts',
     viewer: false,
+  },
+  i18n: {
+    vueI18n: '@/i18n.config.ts',
+    lazy: true,
+    langDir: 'src/i18n',
+    defaultLocale: 'en',
+    strategy: 'no_prefix',
+    dynamicRouteParams: false,
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'un_i18n',
+      // redirectOn: 'root', // recommended
+      cookieCrossOrigin: true,
+    },
+    locales: [
+      { code: 'en', name: 'English', iso: 'en_US', file: 'en_US.ts' },
+      { code: 'ru', name: 'Русский', iso: 'ru_RU', file: 'ru_RU.ts' },
+    ],
   },
 
   devServer,

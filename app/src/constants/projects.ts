986a4c3a118b0cf85018242ea1cb722142dbaf27
Alexandr Lyadov <160845669+unmilley@users.noreply.github.com>
2025-10-16 08:00:10 +0500

    feat: add optisvg to project

--- app/src/constants/projects.ts
+++ app/src/constants/projects.ts
@@ -7,4 +7,12 @@ export const projects: Project[] = [
 		description: 'A scripting notebook for developers! Modify, adapt, convert in safety – now you can do it all!',
 		isDev: false,
 	},
+	{
+		title: 'OptiSVG',
+		image: 'https://optisvg.milley.uno/pwa/favicon.ico',
+		path: 'https://optisvg.milley.uno/',
+		source: 'https://github.com/unmilley/optisvg',
+		description: 'Optimize any SVG in two steps. Free, lossless, and effortless',
+		isDev: false,
+	},
 ]
